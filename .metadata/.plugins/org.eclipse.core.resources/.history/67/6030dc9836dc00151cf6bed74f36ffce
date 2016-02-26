25d086a3a3093f632a7a3dbadbf0136ad095b3b8
Jonahk <jonahkh@uw.edu>
2016-02-25 19:22:35 -0800

    made some changes

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/67/6030dc9836dc00151cf6bed74f36ffce
@@ -0,0 +1,10 @@
+package model;
+
+public class CellToken {
+	
+	/** The column for this cell token. */
+	private int column;
+	
+	/** The row for this cell token. */
+	private int row;
+}
--- Assignment 6B/src/model/CellToken.java
+++ Assignment 6B/src/model/CellToken.java
@@ -7,4 +7,22 @@ public class CellToken {
 	
 	/** The row for this cell token. */
 	private int row;
+	
+	/**
+	 * Returns the column for this cell token.
+	 * 
+	 * @return the column for this cell token
+	 */
+	public int getColumn() {
+		return column;
+	}
+	
+	/**
+	 * Returns the row for this cell token.
+	 * 
+	 * @return the row for this cell token
+	 */
+	public int getRow() {
+		return row;
+	}
 }
