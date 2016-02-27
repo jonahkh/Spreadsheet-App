4a4a43ebd536beda5248d4b25b0e051336bb92df
Jonah <Jonah@Jonah-PC.hsd1.wa.comcast.net>
2016-02-27 12:33:09 -0800

    Added view package, started working on GUI, made some additions to Spreadsheet.java

--- Assignment 6B/src/model/Spreadsheet.java
+++ Assignment 6B/src/model/Spreadsheet.java
@@ -1,32 +1,168 @@
 package model;
 
 public class Spreadsheet {
-	
+
 	/** How many columns are in this spreadsheet. */
 	public static final int COLUMNS = 6;
-	
+
 	/** How many rows are in this spreadsheet. */
 	public static final int ROWS = 6;
 	
+	public static final int LETTERS = 26;
+
 	/** The current spreadsheet. */
 	private final Cell[][] spreadsheet;
 	
-	public Spreadsheet(final int size) {
-		spreadsheet = new Cell[size][size];
+	private final String[] columnNames;
+
+	public Spreadsheet() {
+		spreadsheet = new Cell[ROWS][COLUMNS + 1];
+		columnNames = new String[COLUMNS + 1];
+		fillColumnNames();
 	}
 	
+	private void fillColumnNames() {
+		for (int i = 1; i < COLUMNS + 1; i++) {
+			columnNames[i] = convertToString(i);
+		}
+	}
+	
+	public String[] getColumnNames() {
+		return columnNames;
+	}
+
+	public Cell[][] getSpreadsheet() {
+		return spreadsheet;
+	}
+	
+	public void print() {
+		System.out.println("Hello");
+	}
+
+	public String convertToString(int n) {
+		StringBuilder result = new StringBuilder();
+		do {
+			// Solve rounding issue
+			if (result.length() > 0) {
+				n--;
+			}
+			result.insert(0, (char) ((n % LETTERS) + 65));
+			n /= LETTERS;
+		} while (n > 0);
+		return result.toString();
+	}
+
+	public int convertToInt(String s) {
+		int current = 0;
+		int result = 0;
+		int currentLetter;
+		while (current < s.length()) {
+			if (current > 0) {
+				result++;
+			}
+			currentLetter = s.charAt(current) - 65;
+			result *= LETTERS;
+			result += currentLetter;
+			current++;
+		}
+		return result;
+	}
+
+	int getCellToken(String inputString, int startIndex, CellToken cellToken) {
+		char ch;
+		int column = 0;
+		int row = 0;
+		int index = startIndex;
+
+		// handle a bad startIndex
+		if ((startIndex < 0) || (startIndex >= inputString.length())) {
+			cellToken.setColumn(BadCell);
+			cellToken.setRow(BadCell);
+			return index;
+		}
+
+		// get rid of leading whitespace characters
+		while (index < inputString.length()) {
+			ch = inputString.charAt(index);
+			if (!Character.isWhitespace(ch)) {
+				break;
+			}
+			index++;
+		}
+		if (index == inputString.length) {
+			// reached the end of the string before finding a capital letter
+			cellToken.setColumn(BadCell);
+			cellToken.setRow(BadCell);
+			return index;
+		}
+
+		// ASSERT: index now points to the first non-whitespace character
+
+		ch = inputString.charAt(index);
+		// process CAPITAL alphabetic characters to calculate the column
+		if (!Character.isUpperCase(ch)) {
+			cellToken.setColumn(BadCell);
+			cellToken.setRow(BadCell);
+			return index;
+		} else {
+			column = ch - 'A';
+			index++;
+		}
+
+		while (index < inputString.length()) {
+			ch = inputString.charAt(index);
+			if (Character.isUpperCase(ch)) {
+				column = ((column + 1) * 26) + (ch - 'A');
+				index++;
+			} else {
+				break;
+			}
+		}
+		if (index == inputString.length()) {
+			// reached the end of the string before fully parsing the cell
+			// reference
+			cellToken.setColumn(BadCell);
+			cellToken.setRow(BadCell);
+			return index;
+		}
+
+		// ASSERT: We have processed leading whitespace and the
+		// capital letters of the cell reference
+
+		// read numeric characters to calculate the row
+		if (Character.isDigit(ch)) {
+			row = ch - '0';
+			index++;
+		} else {
+			cellToken.setColumn(BadCell);
+			cellToken.setRow(BadCell);
+			return index;
+		}
+
+		while (index < inputString.length()) {
+			ch = inputString.charAt(index);
+			if (Character.isDigit(ch)) {
+				row = (row * 10) + (ch - '0');
+				index++;
+			} else {
+				break;
+			}
+		}
+
+		// successfully parsed a cell reference
+		cellToken.setColumn(column);
+		cellToken.setRow(row);
+		return index;
+	}
+
 	public void printCellFormula(final CellToken theToken) {
-		
+
 	}
-	
+
 	public void printAllFormulas() {
-		
+
 	}
-	
-	public void getCellToken(String theInputCell, int n, CellToken theToken) {
-		
-	}
-	
+
 	/**
 	 * Returns the number of rows for this spreadsheet.
 	 * 
@@ -35,7 +171,7 @@ public class Spreadsheet {
 	public int getNumRows() {
 		return ROWS;
 	}
-	
+
 	/**
 	 * Returns the number of rows for this spreadsheet.
 	 * 
---
+++ Assignment 6B/src/view/GUI.java
@@ -0,0 +1,35 @@
+package view;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+
+import model.Spreadsheet;
+
+public class GUI {
+	private final JFrame myFrame;
+	private final JTable myTable;
+	private final Spreadsheet mySpreadsheet;
+	
+	public GUI() {
+		myFrame = new JFrame();
+		mySpreadsheet = new Spreadsheet();
+		myTable = new JTable(mySpreadsheet.getSpreadsheet(), mySpreadsheet.getColumnNames());
+		myFrame.add(myTable);
+		myFrame.pack();
+	}
+	
+	public void run() {
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setVisible(true);
+	}
+	
+	
+	
+	
+	
+	
+	public static void main(String... args) {
+		GUI gui = new GUI();
+		gui.run();
+	}
+}
