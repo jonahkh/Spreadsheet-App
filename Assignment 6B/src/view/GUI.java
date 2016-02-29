6f676f6e7dce6fdb5af66fdfce679e76e3c89114
henryl93 <henryl93@IT30154791.insttech.washington.edu>
2016-02-28 18:28:31 -0800

    Added minimum size allowable to be resized. Added title to the window.

--- Assignment 6B/src/view/GUI.java
+++ Assignment 6B/src/view/GUI.java
@@ -9,21 +9,41 @@ import javax.swing.JTable;
 
 import model.Spreadsheet;
 
+/**
+ * This class runs the GUI interface for the spreadsheet application.
+ * 
+ * @author Jonah Howard 
+ * @author Henry Lai
+ */
 public class GUI {
+	/** The minimum width allowed for the window. */
+	private static final int MIN_WIDTH = 400;
+	/** The minimum height allowed for the window. */
+	private static final int MIN_HEIGHT = 300;
+	/** The JFrame that the spreadsheet is displayed on. */
 	private final JFrame myFrame;
+	/** The JScrollPane that the spreadsheet table resides in. */
 	private final JScrollPane myJScrollPane;
+	/** The spreadsheet that contains all the data. */
 	private final Spreadsheet mySpreadsheet;
-
+	
+	/**
+	 * This constructor initializes the GUI interface.
+	 */
 	public GUI() {
-		myFrame = new JFrame();
+		// Sets the title of the program in the title bar.
+		myFrame = new JFrame("TCSS 342 Spreadsheet - Group 8");
 		mySpreadsheet = new Spreadsheet();
+		// Adds the spreadsheet table to a JScrollPane that allows for resizing.
 		myJScrollPane = new JScrollPane(mySpreadsheet.getTable(), 
 				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
 				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		// Adds the scrollable pane to the JFrame.
 		myFrame.add(myJScrollPane, BorderLayout.CENTER);
+		// Sets the minimum size for the window.
+		myFrame.setMinimumSize(new Dimension(MIN_WIDTH, MIN_HEIGHT));
 		myFrame.pack();
 	}
-	
 	
 	public void run() {
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
