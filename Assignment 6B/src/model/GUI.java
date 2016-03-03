29db1c1877eee2aaf31273c7107d301e687eda47
henrylai <henrylai4@gmail.com>
2016-03-03 01:50:51 -0800

    Automatically calculate and resize initial window dimensions based on number of rows and columns.
    Improved implementation of minimum size of myFrame.
    Created a constant for MIN_DIMENSION. 
    Optimized the try/catch block in initialize()

--- Assignment 6B/src/model/GUI.java
+++ Assignment 6B/src/model/GUI.java
@@ -3,6 +3,7 @@ package model;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Observable;
@@ -25,17 +26,32 @@ import javax.swing.JTextField;
  */
 public class GUI extends Observable {
 	
-	/** The minimum width allowed for the window. */
-	private static final int MIN_WIDTH = 400;
+	/** The minimum number of rows and columns allowed. */
+	private static final int MIN_DIMENSION = 3;
+
+	/** Horizontal offset for initial frame resizing. */
+	private static final int HORIZONTAL_OFFSET = 49;
 	
-	/** The minimum height allowed for the window. */
-	private static final int MIN_HEIGHT = 300;
+	/** Vertical offset for initial frame resizing. */
+	private static final int VERTICAL_OFFSET = 97;
+	
+	/** The width of a cell in pixels. Default = 75 */
+	private static final int CELL_WIDTH = 75;
+
+	/** The height of a cell in pixels. Default = 16 */
+	private static final int CELL_HEIGTH = 16;
 	
 	/** The JFrame that the spreadsheet is displayed on. */
 	private final JFrame myFrame;
 	
 	/** The spreadsheet that contains all the data. */
 	private final Spreadsheet mySpreadsheet;
+	
+	/** The user-inputed number of rows in the spreadsheet. */
+	private int rows;
+	
+	/** The user-inputed number of columns in the spreadsheet. */
+	private int cols;
 
 	/**
 	 * This constructor initializes the GUI interface.
@@ -44,10 +60,45 @@ public class GUI extends Observable {
 		// Sets the title of the program in the title bar.
 		myFrame = new JFrame("TCSS 342 Spreadsheet - Group 8");
 		final Dimension dimension = initialize();
-		mySpreadsheet = new Spreadsheet((int) dimension.getWidth(), 
-				(int) dimension.getHeight());
+		mySpreadsheet = new Spreadsheet((int) dimension.getWidth(), (int) dimension.getHeight());
+		resizeComponents();
 	}
 	
+	/**
+	 * This method adjusts the initial and minimum component size based on the
+	 * number of rows and columns.
+	 */
+	private void resizeComponents() {
+		// Calculate the width and height of all components based on table dimensions.
+		final int newWidth = (HORIZONTAL_OFFSET + CELL_WIDTH * cols);
+		final int newHeight = (VERTICAL_OFFSET + CELL_HEIGTH * rows);
+		
+		// Gets the screen resolution of user's system.
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		final double width = screenSize.getWidth();
+		final double height = screenSize.getHeight();
+		
+		// Sets the initial window size to be the minimum of either screen resolution or calculated dimensions.
+		myFrame.setSize((int) Math.min(newWidth, width), (int) Math.min(newHeight, height));
+		
+		// Sets the minimum window size to the the minimum of 6 columns and 8 rows unless it is smaller.
+		final int minWidth = (int) Math.min(newWidth, HORIZONTAL_OFFSET + CELL_WIDTH * 6);
+		final int minHeight = (int) Math.min(newHeight, VERTICAL_OFFSET + CELL_HEIGTH * 8);
+		
+		// Sets the maximum window size to be the table dimension unless it is larger than resolution.
+		/* This currently doesn't work.
+		int maxWidth = newWidth;
+		int maxHeight = newHeight;
+		if (newWidth > width) {
+			maxWidth = (int) width;
+		}
+		if (newHeight > height) {
+			maxHeight = (int) height;
+		} 
+		myFrame.setMaximumSize(new Dimension(maxWidth, maxHeight)); */
+		myFrame.setMinimumSize(new Dimension(minWidth, minHeight));
+	}
+
 	/**
 	 * Prompts the user to enter the desired size of the spread sheet.
 	 * 
@@ -68,7 +119,9 @@ public class GUI extends Observable {
 		JPanel bottomPanel = new JPanel();
 
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		panel.add(new JLabel("Minimum size is 3x3")); // This is asserted
+		JPanel textPanel = new JPanel();
+		textPanel.add(new JLabel("Minimum size is 3x3."));
+		panel.add(textPanel); // This is asserted
 		
 		bottomPanel.add(new JLabel("Rows:"));
 		bottomPanel.add(rowSize);
@@ -81,24 +134,18 @@ public class GUI extends Observable {
 		try {
 			int result = JOptionPane.showConfirmDialog(myFrame, panel, 
 					"Please enter the size of the spreadsheet:", 
-					JOptionPane.OK_CANCEL_OPTION);
+					JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 			if (result == JOptionPane.OK_OPTION) {
-				int col = Integer.parseInt(columnSize.getText());
-				int row = Integer.parseInt(rowSize.getText());
-				// Assert the minimum size of 3x3
-				if (col < 3) {
-					col = 3;
-				}
-				if (row < 3) {
-					row = 3;
-				}
-				dim.setSize(row, col);
+				cols = Integer.parseInt(columnSize.getText());
+				rows = Integer.parseInt(rowSize.getText());
 			} else {
 				System.exit(0);	// Terminate the program
 			}
 		} catch (NumberFormatException e) {	// If invalid input is entered
-			dim.setSize(3, 3);
+			// Assert the minimum size of 3x3
+			rows = cols = MIN_DIMENSION;
 		}
+		dim.setSize(cols, rows);
 		return dim;
 	}
 
@@ -111,10 +158,10 @@ public class GUI extends Observable {
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// Adds the scrollable pane to the JFrame to enable the scrollbar
 		myFrame.add(new JScrollPane(mySpreadsheet.getTable(), 
-				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
-				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS), BorderLayout.CENTER);
+				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
 		// Sets the minimum size for the window.
-		myFrame.setMinimumSize(new Dimension(MIN_WIDTH, MIN_HEIGHT));
+		//myFrame.setMinimumSize(new Dimension(MIN_WIDTH, MIN_HEIGHT));
 		myFrame.setLocationRelativeTo(null);
 		
 		// Add buttons to the frame
@@ -129,11 +176,8 @@ public class GUI extends Observable {
 		panel.add(values, BorderLayout.SOUTH);
 		
 		myFrame.add(panel, BorderLayout.SOUTH);
-		myFrame.pack();
 		myFrame.setVisible(true);
 	}
-	
-	
 	
 	/**
 	 * Adds the action listeners for the "Display Values" and "Display Formulas" buttons.
